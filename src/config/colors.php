aec4d7bf384a97c248ca8e32cccaef25e946f173
Olav <olav@web64.com>
2019-02-09 21:47:43 +0100

    wip

--- src/config/colors.php
+++ src/config/colors.php
@@ -14,4 +14,73 @@ return [
     'created'   => ['black', 'bg_green'],
     'updated'   => ['black', 'bg_yellow'],
     'deleted'   => ['white', 'bg_red'],
+
+    'bgRed'         => ['white', 'bg_red'],
+    'bgGreen'       => ['white', 'bg_green'],
+    'bgYellow'      => ['white', 'bg_yellow'],
+    'bgBlue'        => ['white', 'bg_blue'],
+    'bgMagenta'     => ['white', 'bg_magenta'],
+    'bgCyan'        => ['white', 'bg_cyan'],
+    'bgLightGray'   => ['white', 'bg_ligh_gray'],
+    'bgLightRed'    => ['white', 'bg_light_red'],
+    'bgLightGreen'  => ['white', 'bg_light_green'],
+    'bgLightYellow' => ['white', 'bg_light_yellow'],
+    'bgLightBlue'   => ['white', 'bg_light_blue'],
+    'bgLightMagenta'=> ['white', 'bg_light_magenta'],
+    'bgLightCyan'   => ['white', 'bg_light_cyan'],
+    'bgWhite'       => ['white', 'bg_white'],
+
+
 ];
+
+/** Available options
+
+* Styles:
+none
+bold
+dark
+italic
+underline
+blink
+reverse
+concealed
+
+* Text Color:
+default
+black
+red
+green
+yellow
+blue
+magenta
+cyan
+light_gray
+dark_gray
+light_red
+light_green
+light_yellow
+light_blue
+light_magenta
+light_cyan
+white
+
+* Background Color
+bg_default
+bg_black
+bg_red
+bg_green
+bg_yellow
+bg_blue
+bg_magenta
+bg_cyan
+bg_light_gray
+bg_dark_gray
+bg_light_red
+bg_light_green
+bg_light_yellow
+bg_light_blue
+bg_light_magenta
+bg_light_cyan
+bg_white
+
+*/
